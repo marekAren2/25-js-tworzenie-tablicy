5413b7cc21b4a894190d6007502382e37b615f35
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-12 19:59:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+/* Opis
+
+1. Stwórz plik main.js
+2. Stwórz zamienną o dowolnej nazwie, przypisz do niej tablicę, w której umieść dowolną ilość liczb.
+
+Dodaj plik do repozytorium zdalnego i podeślij link do sprawdzenia.
+ */
